82bfd241a5e30f6a2f66b8ce92e9e163f16baa05
Александр <sasha12345678_92@mail.ru>
2023-05-19 13:00:47 +0300

    изменил принцип получения размера списка, добавил обработку отрицательного индекса, зациклил список, попробовал реализовать принцип бинарного поиска

--- list.cpp
+++ list.cpp
@@ -1,82 +1,109 @@
 #include <iostream>
+#include <math.h>
 
 struct Node{
     Node* prev;
     Node* next;
     int val;
-    int current_count;
-    Node(int value, Node* &previos, Node* &next_, int cur): val{value}, prev{previos}, next{next_}, current_count{cur} {};
-    Node(int value, Node* &previos, int cur): val{value}, prev{previos}, next{nullptr}, current_count{cur} {};
-    Node(int value): val{value}, prev{nullptr}, next{nullptr}, current_count{0} {};
+    Node() = default;
+    Node(int value, Node* &previos, Node* &next_): val{value}, prev{previos}, next{next_}{};
+    Node(int value, Node* &previos): val{value}, prev{previos}, next{nullptr} {};
+    Node(int value): val{value}, prev{nullptr}, next{nullptr}{};
     ~Node() = default;
 };
 
 class list_container{
     public:
-        list_container(): first{nullptr}, prev{nullptr}, last{nullptr} {}
+        list_container(): first{nullptr}, last{nullptr}, size_{0}{}
         ~list_container() = default;
         
         int size(){
-            int lenght = last->current_count + 1;
-            return lenght;
+            return size_;
         }
         
+        Node* find_from_beginning(Node* element, int count_iter){
+            for (size_t i=0; i < count_iter; ++i){
+                            element = element->next;
+            }
+            return element;
+        }
+
+        Node* find_from_end(Node* element, int count_iter){
+            for (size_t i=1; i < count_iter; ++i){
+                            element = element->prev;
+            }
+            return element;
+        }
+
 
         Node* find_element(int count){
-            if (count > last->current_count){
-                std::cout<<"Out of memory"<<std::endl;
-                return nullptr;
-            }
+            int middle = (size_ / 2);
             Node* elem = first;
-            while(elem->current_count != count){
-                elem = elem->next;
+            if (count >=0){
+                if (count <= size_){
+                    if (count < middle){
+                        elem = find_from_beginning(first, count);
+                    }
+                    else{
+                        int find_count = size_ - count;
+                        elem = find_from_end(last, find_count);
+                    }
+                }
+                else{
+                    elem = last;
+                }
+            }
+            else{
+                int abs_count = abs(count);
+                if (abs_count <= size_){
+                    if (abs_count < middle){
+                        elem = find_from_end(last, abs_count);
+                    }
+                    else{
+                        int find_count = size_ - abs_count;
+                        elem = find_from_beginning(first, find_count);
+                    }
+                }
+                else{
+                    elem = first;
+                }
             }
             return elem;
         }
+
 
         int operator[](const int index){
             Node* elem = find_element(index);
             return elem->val;
         }
 
-        void correct_index(Node* element, char math){
-            while(element != last){
-                if(math == '+'){element->current_count += 1;}
-                else{element->current_count -= 1;}
-                element = element->next;
-            }
-            if(math == '+'){last->current_count += 1;}
-            else{last->current_count -= 1;}
-        }
-
         void push_back(int val){
             Node* elem = new Node(val);
             if(first == nullptr){
+                elem->next = elem;
+                elem->prev = elem;
                 first = elem;
-                prev = elem;
                 last = elem;
+                ++size_;
             }
             else{
+                elem->prev = last;
+                elem->next = first;
                 last->next = elem;
-                prev = last;
                 last = elem;
-                last->prev = prev;
-                last->current_count = prev->current_count + 1;
+                ++size_;
             };
         }
         void erase(int index){
             Node* del_elem = find_element(index);
-            std::cout<<del_elem->val<<std::endl;
             if (del_elem == first){
                 Node* next_del_elem = del_elem->next;
-                std::cout<<next_del_elem->val<<std::endl;
-                next_del_elem->prev = nullptr;
+                next_del_elem->prev = last;
                 first = next_del_elem;
-                correct_index(next_del_elem, '-');
             }
             else if (del_elem == last){
                 Node* prev_del_elem = del_elem->prev;
-                prev_del_elem->next = nullptr;
+                prev_del_elem->next = first;
                 last = prev_del_elem;
             }
             else{
@@ -84,32 +111,31 @@ class list_container{
                 Node* prev_del_elem = del_elem->prev;
                 prev_del_elem->next = next_del_elem;
                 next_del_elem->prev = prev_del_elem;
-                correct_index(next_del_elem, '-');
             }
             delete del_elem;
+            --size_;
         }
 
         void insert(int index, int value){
-            int size_ = size();
+            int max_val = size();
             if (index == 0){
                 Node* moving_element = first;
                 Node* insert_element = new Node(value);
-                insert_element->prev = nullptr;
+                insert_element->prev = last;
                 insert_element->next = moving_element;
                 first = insert_element;
-                correct_index(moving_element, '+');
             }
-            else if(index >= size_){
+            else if(index >= max_val){
                 push_back(value);
             }
             else{
                 Node* moving_element = find_element(index);
                 Node* prev_moving_element = moving_element->prev;
-                Node* insert_element = new Node(value, prev_moving_element, moving_element, index);
+                Node* insert_element = new Node(value, prev_moving_element, moving_element);
                 prev_moving_element->next = insert_element;
                 moving_element->prev = insert_element;
-                correct_index(moving_element, '+');
             }
+            ++size_;
             
         }
 
@@ -126,8 +152,8 @@ class list_container{
 
     private:
         Node* first;
-        Node* prev;
         Node* last;
+        int size_;
 
 };
 
@@ -147,6 +173,7 @@ int main(){
     lc.push_back(8);
     lc.push_back(9);
     lc.print();
+    std::cout<<lc.size()<<std::endl;
     lc.erase(2);
     lc.erase(3);
     lc.erase(4);
